1eb76708b9ac763404b6f1102715b4634a7a4e23
Abdubek <zholay.abdubek@gmail.com>
2024-03-18 01:49:15 +0500

    fix layout

--- src/app/cabinet/layout.tsx
+++ src/app/cabinet/layout.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import {Routes} from "@/routes";
 import {ParticipantCard} from "@/entities/participant/ui/Card";
 import {ProfileMenu} from "@/entities/participant/ui/ProfileMenu";
+import {Footer} from "@/app/_sections/Footer";
 
 
 export default function CabinetLayout({
@@ -16,7 +17,9 @@ export default function CabinetLayout({
     <div>
       <header className="bg-bg-primary">
         <div className="container py-2.5 flex justify-between items-center">
-          <ForegroundLogo width={48} height={48} />
+          <Link href={Routes.CABINET}>
+            <ForegroundLogo width={48} height={48} />
+          </Link>
 
           <Button asChild size="sm" radius="md" weight="bold" variant="foreground">
             <Link href={Routes.HOME}>
@@ -26,7 +29,7 @@ export default function CabinetLayout({
         </div>
       </header>
 
-      <div className="container grid grid-cols-4 gap-15 pt-15">
+      <div className="container grid grid-cols-4 gap-15 pt-15 mb-48">
         <div className="col-span-1 flex flex-col gap-6">
           <ParticipantCard />
           <ProfileMenu />
@@ -35,6 +38,8 @@ export default function CabinetLayout({
           {children}
         </div>
       </div>
+
+      <Footer />
     </div>
   );
 }
