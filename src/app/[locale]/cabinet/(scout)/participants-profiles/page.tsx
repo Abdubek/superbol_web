0627ef2a0a9579b3846ddaeddc28ed50f44d2a02
Abdubek <zholay.abdubek@gmail.com>
2024-04-07 19:37:23 +0500

    add favorite filter

--- src/app/[locale]/cabinet/(scout)/participants-profiles/page.tsx
+++ src/app/[locale]/cabinet/(scout)/participants-profiles/page.tsx
@@ -8,6 +8,7 @@ import {cn} from "@/shared/utils/common";
 import {citiesApi} from "@/shared/api/cities";
 import {CastingCityFilter} from "@/features/CastingCityFilter";
 import {SearchParams} from "nuqs/parsers";
+import {FavoriteFilter} from "@/features/FavoriteFilter";
 
 type Props = {
   searchParams: SearchParams;
@@ -16,12 +17,14 @@ type Props = {
 export default async function CabinetParticipantProfilesPage({ searchParams }: Props) {
 
   const city = searchParams.city as string | undefined
+  const by = searchParams.by as string | undefined
 
   const [cities, data] = await Promise.all([
     citiesApi.getCitiesList(),
     participantApi.getParticipantsList({
       status: "application_verified",
-      casting_city: city
+      casting_city: city,
+      only_faves: by === "favorite"
     })
   ])
 
@@ -29,44 +32,49 @@ export default async function CabinetParticipantProfilesPage({ searchParams }: P
     <main>
       <Typography size="h3" className="mb-10">Список участников</Typography>
       <CastingCityFilter cities={cities} className="mb-10" />
-      <div className="overflow-x-scroll">
-        <table className="w-full whitespace-nowrap">
-          <thead>
-          <tr className="bg-bg-platinum font-semibold text-text-darkblue">
-            <th className="px-6 py-4"></th>
-            <th className="px-6 py-4 text-left">Имя Фамилия</th>
-            <th className="py-4 text-left">Оценка</th>
-            <th className="px-6 py-4 text-left">Город</th>
-            <th className="px-6 py-4 text-left">Игровая позиция 1</th>
-            <th className="px-6 py-4 text-left">Игровая позиция 2</th>
-            <th className="px-6 py-4 text-left">Игровая позиция 3</th>
-          </tr>
-          </thead>
-          <tbody>
-          {data?.map((item, index) =>
-            <tr key={index} className="border-b border-border-lightgray">
-              <td className="px-6 py-4">
-                <FavoriteButton isFavorite={item.is_favorite} participantId={item.id} />
-              </td>
-              <td className="px-6 py-4 flex items-center gap-4">
-                <div>
-                  <DefaultAvatar />
-                </div>
-                {item.full_name}
-              </td>
-              <td>
-                <RatingInput defaultValue={item.rating} participantId={item.id} />
-              </td>
-              <td className="px-6 py-4">{item.casting_city}</td>
-              {item.gaming_positions?.map((pos, posInd) =>
-                <td key={posInd} className="px-6 py-4">
-                  <GamePosition pos={pos} />
-                </td>
-              )}
+      <div className="border border-border-lightgray rounded-md p-4 flex flex-col gap-2">
+        <div>
+          <FavoriteFilter />
+        </div>
+        <div className="overflow-x-scroll">
+          <table className="w-full whitespace-nowrap">
+            <thead>
+            <tr className="bg-bg-platinum font-semibold text-text-darkblue">
+              <th className="px-6 py-4"></th>
+              <th className="px-6 py-4 text-left">Имя Фамилия</th>
+              <th className="py-4 text-left">Оценка</th>
+              <th className="px-6 py-4 text-left">Город</th>
+              <th className="px-6 py-4 text-left">Игровая позиция 1</th>
+              <th className="px-6 py-4 text-left">Игровая позиция 2</th>
+              <th className="px-6 py-4 text-left">Игровая позиция 3</th>
             </tr>
-          )}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+            {data?.map((item, index) =>
+              <tr key={index} className="border-b border-border-lightgray">
+                <td className="px-6 py-4">
+                  <FavoriteButton isFavorite={item.is_favorite} participantId={item.id} />
+                </td>
+                <td className="px-6 py-4 flex items-center gap-4">
+                  <div>
+                    <DefaultAvatar />
+                  </div>
+                  {item.full_name}
+                </td>
+                <td>
+                  <RatingInput defaultValue={item.rating} participantId={item.id} />
+                </td>
+                <td className="px-6 py-4">{item.casting_city}</td>
+                {item.gaming_positions?.map((pos, posInd) =>
+                  <td key={posInd} className="px-6 py-4">
+                    <GamePosition pos={pos} />
+                  </td>
+                )}
+              </tr>
+            )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </main>
   )
---
+++ src/features/FavoriteFilter.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import {Button} from "@/shared/ui/Button";
+import {useQueryState} from "nuqs";
+
+export const FavoriteFilter = () => {
+  const [value, setValue] = useQueryState('by', {
+    defaultValue: "all"
+  })
+
+  return (
+    <div className="flex gap-1 items-center">
+      <Button size="sm" variant={value === "all" ? "primary" : "secondary"} onClick={() => setValue("all", { shallow: true })}>Все участники</Button>
+      <Button size="sm" variant={value === "favorite" ? "primary" : "secondary"} onClick={() => setValue("favorite", { shallow: true })}>Избранные</Button>
+    </div>
+  )
+}
