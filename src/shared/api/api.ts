c1cc1d598185ebddf01784e100e497d645d53406
Abdubek <zholay.abdubek@gmail.com>
2024-04-11 10:14:23 +0500

    fix

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,8 +4,8 @@ import {setFlash} from "@/features/FlashToaster/FlashToaster";
 import {getLocale} from "next-intl/server";
 
 // const API_URL = 'http://46.101.124.209:8082/api/v1'
-const API_URL = 'https://super-bol.kz/api/v1'
-// const API_URL = 'http://super-bol.kz:9090/api/v1'
+// const API_URL = 'https://super-bol.kz/api/v1'
+const API_URL = 'http://super-bol.kz:9090/api/v1'
 
 type BaseResponse<T> = {
   data: T
