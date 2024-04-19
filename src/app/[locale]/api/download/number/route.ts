413e0cee74653304029624fcf7664575686886c2
Abdubek <zholay.abdubek@gmail.com>
2024-04-19 19:48:26 +0500

    add download number

--- src/app/[locale]/api/download/number/route.ts
+++ src/app/[locale]/api/download/number/route.ts
@@ -1,6 +1,14 @@
 import {participantApi} from "@/shared/api/participant";
+import {NextResponse} from "next/server";
 
 export async function GET() {
   const res = await participantApi.numberPdf()
-  return new Response(res)
+
+  const headers = new Headers()
+  headers.set('Content-Type', 'application/pdf')
+  return new NextResponse(res, {
+    status: 200,
+    statusText: "OK",
+    headers
+  })
 }
